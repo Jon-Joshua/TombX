79827757a367b314dc0c09c82cce95df5f834649
JJ Wyatt <jon-joshua@live.com>
2018-07-30 18:55:23 +0100

    Inital commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # TombX
+
+### About
+
+TombX is an open source re-implmentation of Tomb Raider 1.
+
+### Running
+
+I've only ran TombX with the [Automated Fix](https://github.com/Carlmundo/TombRaider-AutomatedFix/releases/tag/release). To run the game replace `tombati.exe` and add the `TombX.dll` supplied. The game currently only boots up and keyboard input currently isn't working.
--- src/Addresses.hpp
+++ src/Addresses.hpp
@@ -9,17 +9,6 @@
 #define TOMB_CALLPROC(address)					(((void(*)())address)())
 #define TOMB_CALLFUNC(address, returnType)		(((returnType(*)())address)())
 
-template<class T, class... Types>
-inline T(*TOMB_PROC_PTR(const std::uintptr_t address))(Types...) {
-	return reinterpret_cast<T(__cdecl *)(Types...)>(address);
-}
-
-#define TOMB_SUB_LOAD_PCX (TOMB_PROC_PTR<void, char[]>(TOMB_ADDRESS_LOAD_PCX))
-#define TOMB_SUB_PLAY_FMV (TOMB_PROC_PTR<void, int, int>(TOMB_ADDRESS_PLAY_FMV))
-#define TOMB_SUB_416550 (TOMB_PROC_PTR<void, int, double>(0x00416550))
-#define TOMB_SUB_4344D0 (TOMB_PROC_PTR<void, int, int>(0x004344D0))
-#define TOMB_SUB_KEYBOARDHOOK (TOMB_PROC_PTR<void, int, WPARAM, LPARAM>(0x0043D8C0))
-
 #define TOMB_ADDRESS_PLAY_FMV					0x0041CDF0
 #define TOMB_ADDRESS_LOAD_PCX					0x00430CE0
 #define TOMB_ADDRESS_CONTROLS					0x00454A22
